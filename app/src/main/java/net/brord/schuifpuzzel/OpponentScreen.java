2d9e78333dcbf8db69736912c68a2fbad2d40478
kwek20 <brordvanwierst@msn.com>
2015-06-19 11:45:02 +0200

    Added loading stuff

--- app/src/main/java/net/brord/schuifpuzzel/OpponentScreen.java
+++ app/src/main/java/net/brord/schuifpuzzel/OpponentScreen.java
@@ -1,8 +1,12 @@
 package net.brord.schuifpuzzel;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.text.Editable;
 import android.util.Log;
@@ -14,6 +18,11 @@ import android.widget.TextView;
  * Created by Brord on 6/18/2015.
  */
 public class OpponentScreen extends ActionBarActivity {
+
+    private static final String LOADER_TAG = "Loader";
+    private String username;
+
+    private LoaderDialog loader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,18 +52,49 @@ public class OpponentScreen extends ActionBarActivity {
 
     public void createGame(View v){
         if (!checkUsername())return;
+
+
     }
 
     private boolean checkUsername() {
         TextView group = (TextView) findViewById(R.id.userName);
-        Log.d("MAD", "name: " + group.getText());
-        if (!group.getText().equals("firebasecheckhere")) return true;
 
+        if (!(username = group.getText().toString()).equals("test")) return true;
         new AlertDialog.Builder(OpponentScreen.this)
                 .setTitle(R.string.error)
                 .setMessage(getString(R.string.unavailable))
-        .show();
-
+                .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        waitForNotification();
+                    }
+                }).show();
         return false;
+    }
+
+    public void waitForNotification() {
+        (loader = new LoaderDialog()).show(getFragmentManager(), LOADER_TAG);
+    }
+
+    public void doneLoading() {
+
+        if (loader != null && loader.getShowsDialog()) {
+            loader.dismiss();
+        }
+    }
+
+    public static class LoaderDialog extends DialogFragment {
+
+        public LoaderDialog(){}
+
+        @Override
+        public Dialog onCreateDialog(final Bundle savedInstanceState) {
+
+            ProgressDialog _dialog = new ProgressDialog(getActivity());
+            this.setStyle(STYLE_NO_TITLE, getTheme()); // You can use styles or inflate a view
+            _dialog.setMessage(getString(R.string.searching)); // set your messages if not inflated from XML
+            _dialog.setCancelable(false);
+
+            return _dialog;
+        }
     }
 }
